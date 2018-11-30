16c1f8f4bc440d7b4c0c72b7fc218a051a631052
Riley Stonehouse <riley.stonehouse@gmail.com>
2018-11-30 18:30:46 +0000

    Added validations.

--- app/models/product_ranking.rb
+++ app/models/product_ranking.rb
@@ -2,4 +2,7 @@ class ProductRanking < ApplicationRecord
   # number: integer
   # category: string
   belongs_to :product
+
+  validates :number, numericality: true
+  validates :category, presence: true
 end
